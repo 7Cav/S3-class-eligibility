b9704d5963f619d8196b115f71d74cb596c797ed
Olaf de Mol <olafdemol@hotmail.com>
2018-02-25 17:15:25 +0100

    Splash screen

--- Applications/ace-tool/src/App.vue
+++ Applications/ace-tool/src/App.vue
@@ -2,12 +2,17 @@
   <v-app>
     <v-content>
       <router-view/>
+      <rotate-square2></rotate-square2>
     </v-content>
   </v-app>
 </template>
 
 <script>
+import {RotateSquare2} from 'vue-loading-spinner'
 export default {
+  components: {
+    RotateSquare2
+  },
   data () {
     return {
       clipped: false,
--- Applications/ace-tool/src/components/LoadScreen.vue
+++ Applications/ace-tool/src/components/LoadScreen.vue
@@ -1,24 +1,58 @@
 <template>
-<div class="overlay">
- <div class="wrapper">
-      <div class="rectangle-wrapper">
-        <div class="rectangle-banner">
-                <p>bla</p>
+<div>
+    <div class="overlay">
+        <div class="wrapper">
+            <div class="rectangle-wrapper">
+                <div class="rectangle-banner">
+                
+                </div>
             </div>
-      </div>
-  </div>
+        </div>
+    </div>
+    <div>
+        <div class="loading-text">
+            <h1>Loading MILPACS...</h1>
+        </div>
+        <Circle8 class="spinner"></Circle8>
+    </div>
 </div>
+
  
 </template>
 
 <script>
+import {Circle8} from 'vue-loading-spinner'
 export default {
-
+    components: {
+    Circle8
+  }
 }
 </script>
 
 <style lang="scss">
 @import "../style/_variables";
+
+.loading-text{
+    position: absolute;
+    left:50%;
+    top:50%;
+    color:white;
+    z-index: 5;
+    transform: translate(-50%, -50%);
+    text-align: center;
+}
+
+.spinner{ 
+    position: absolute;
+    bottom:33%;
+    left:50%;
+    transform: translateX(-50%);
+    z-index:10;
+        .spinner-inner{
+            transform:scale(2) !important;
+            margin-top:5em !important;
+        }
+    }
 
 .overlay{
     opacity: 49%;
@@ -36,16 +70,19 @@ export default {
             position: relative;
             overflow: hidden;
             .rectangle-banner {
-                    position: absolute;
-                    right:0;
-                    left:0;
-                    height:100%;
-                    background-color: black;
-                    transform: skewY(45deg);
+                position: absolute;
+                right:0;
+                left:0;
+                height:100%;
+                background-color: black;
+                transform: skewY(45deg);
+
+                
             }
         }
     }
 }
+
 
 </style>
 
