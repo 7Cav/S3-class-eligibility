0b7ac8882f276b76fcae857fe3df9c7540a87ce7
Olaf de Mol <olafdemol@hotmail.com>
2018-03-01 17:48:35 +0100

    fix

--- Applications/ace-tool/src/router/index.js
+++ Applications/ace-tool/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/components/Home'
 import LoadingScreen from '@/components/LoadingScreen'
 import SelectClass from '@/components/SelectClass'
 
@@ -8,11 +7,6 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/Home',
-      name: 'Home',
-      component: Home
-    },
     {
       path: '/',
       name: 'SelectClass',
